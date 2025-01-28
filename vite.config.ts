d74a84e630e1ca947438526ca08ef039cc3890b8
leandro-M <leandrotinfo19@gmail.com>
2025-01-27 22:26:41 -0300

    fix: adjust base url to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api.deezer.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(() => {
+  const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
+
+  return {
+    base: isGitHubActions ? '/technical-test-deezer/' : '/',
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: 'https://api.deezer.com',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
+  };
 });
